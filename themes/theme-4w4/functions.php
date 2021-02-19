1f45bad136bda9c4c55ab9409df12339e2bb384b
Jonathan Rivest <jonathan_romero@hotmail.fr>
2021-02-19 16:13:08 +0000

    Modifications du labo 3 et readme.md

--- themes/theme-4w4/functions.php
+++ themes/theme-4w4/functions.php
@@ -139,8 +139,20 @@ add_action( 'widgets_init', 'theme_4w4_widgets_init' );
 /**
  * Enqueue scripts and styles.
  */
+/*
+wp_register_style
+echo "<pre>";
+echo filemtime(get_template_directory(). "/style.css");
+echo "</pre>";
+wp_die();
+*/
+
 function theme_4w4_scripts() {
-	wp_enqueue_style( 'theme-4w4-style', get_stylesheet_uri(), array(), _S_VERSION );
+	
+	wp_register_style( 'theme-4w4-style', get_stylesheet_uri(), array(), filemtime(get_template_directory(). "/style.css"), 'all' );
+	
+	wp_enqueue_style( 'theme-4w4-style');
+	
 	wp_style_add_data( 'theme-4w4-google-fonts', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300&display=swap', false );
 
 	wp_enqueue_script( 'theme-4w4-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
